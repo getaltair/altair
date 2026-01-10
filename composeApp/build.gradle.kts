e18fba5893eb49376dd89cc9fc510ddb45cbb678
Robert Hamilton <robert@rghsoftware.com>
2026-01-09 22:36:51 -0600

    refactor: restructure to separate Android app from KMP library
    Architecture changes per Android KMP migration guide:
    - Create new androidApp module for pure Android application code
    - Move MainActivity, AndroidManifest, and resources to androidApp
    - Convert composeApp from hybrid to KMP library using new plugin
    - composeApp now uses com.android.kotlin.multiplatform.library
    - androidApp depends on composeApp for shared UI code
    
    This separates concerns per the official migration guide:
    - KMP libraries should not use com.android.application
    - Android apps should be pure Android modules depending on KMP libs
    
    Build verified: androidApp:assembleDebug and composeApp:assemble pass

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,19 +3,23 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidKotlinMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
 }
 
 kotlin {
-    androidTarget {
+    androidLibrary {
+        namespace = "com.getaltair.altair.composeapp"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosArm64(),
         iosSimulatorArm64()
@@ -25,13 +29,12 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     jvm()
-    
+
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -52,37 +55,6 @@ kotlin {
             implementation(libs.kotlinx.coroutinesSwing)
         }
     }
-}
-
-android {
-    namespace = "com.getaltair.altair"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = "com.getaltair.altair"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-dependencies {
-    debugImplementation(compose.uiTooling)
 }
 
 compose.desktop {
