7d395692d8387bb12cef5f707437c29b0fc7f4aa
Robert Hamilton <robert@rghsoftware.com>
2026-01-14 19:14:12 -0600

    feat: initial infrastructure

---
+++ server/src/main/kotlin/Application.kt
@@ -0,0 +1,15 @@
+package com.getaltair
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureSecurity()
+    configureMonitoring()
+    configureHTTP()
+    configureRouting()
+}
